fd76ba107528a8d1e80c9ccbb1869d0c47670aed
Gabriel Bene <165289683+gbene00@users.noreply.github.com>
2025-12-05 09:41:20 +0200

    fixing inbound connectivity

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -15,34 +15,3 @@ resource "azurerm_subnet" "aks" {
 
   depends_on = [azurerm_virtual_network.vnet]
 }
-
-## Azure Network Security Group for AKS Subnet
-resource "azurerm_network_security_group" "aks_nsg" {
-  name                = var.aks_subnet_nsg_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
-## Associate NSG to AKS Subnet
-resource "azurerm_subnet_network_security_group_association" "aks_subnet_nsg_assoc" {
-  subnet_id                 = azurerm_subnet.aks.id
-  network_security_group_id = azurerm_network_security_group.aks_nsg.id
-}
-
-## NSG rules for the AKS subnet NSG
-resource "azurerm_network_security_rule" "aks_subnet_rules" {
-  for_each = { for rule in var.aks_subnet_nsg_rules : rule.name => rule }
-
-  name                       = each.value.name
-  priority                   = each.value.priority
-  direction                  = each.value.direction
-  access                     = each.value.access
-  protocol                   = each.value.protocol
-  source_port_range          = each.value.source_port_range
-  destination_port_range     = each.value.destination_port_range
-  source_address_prefix      = each.value.source_address_prefix
-  destination_address_prefix = each.value.destination_address_prefix
-
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.aks_nsg.name
-}
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -33,39 +33,3 @@ variable "aks_subnet_prefix" {
   description = "AKS subnet CIDR prefix"
   type        = string
 }
-
-## Network Security Group name for AKS subnet
-variable "aks_subnet_nsg_name" {
-  description = "NSG name for the AKS subnet"
-  type        = string
-}
-
-## NSG rules for the AKS subnet NSG
-variable "aks_subnet_nsg_rules" {
-  description = "List of NSG rules to apply to the AKS subnet NSG"
-  type = list(object({
-    name                       = string
-    priority                   = number
-    direction                  = string
-    access                     = string
-    protocol                   = string
-    source_port_range          = string
-    destination_port_range     = string
-    source_address_prefix      = string
-    destination_address_prefix = string
-  }))
-
-  default = [
-    {
-      name                       = "Allow-AzureLB-All"
-      priority                   = 100
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "*"
-      source_port_range          = "*"
-      destination_port_range     = "*"
-      source_address_prefix      = "AzureLoadBalancer"
-      destination_address_prefix = "*"
-    }
-  ]
-}
